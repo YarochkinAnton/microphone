7c435825dbdf3ce4b8e20ee00f4db1eba792b627
YarochkinAnton <yarochkin.anton@yandex.ru>
2022-09-10 03:22:42 +0300

    Rough implementation
    Posting file with message is implemented
    TODO: Clean up the mess
    TODO: Update documentation

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,25 @@ use std::{
 
 use actix_web::{
     dev::ConnectionInfo,
+    guard,
+    http::header,
     middleware::Logger,
-    web,
+    web::{
+        self,
+        PayloadConfig,
+    },
     App,
     HttpResponse,
     HttpServer,
     Responder,
 };
+use futures::StreamExt;
 use ipnet::IpNet;
 use reqwest::{
+    multipart::{
+        Form,
+        Part,
+    },
     ClientBuilder,
     StatusCode,
 };
@@ -25,7 +35,8 @@ use serde::{
 
 const TELEGRAM_API_BASE_URL: &str = "https://api.telegram.org";
 const TELEGRAM_SEND_MESSAGE_METHOD: &str = "sendMessage";
-const TELEGRAM_SEND_MESSAGE_PARSE_MODE: &str = "MarkdownV2";
+const TELEGRAM_SEND_DOCUMENT_METHOD: &str = "sendDocument";
+const TELEGRAM_MARKDOWN_V2_PARSE_MODE: &str = "MarkdownV2";
 
 type Topics = HashMap<String, Topic>;
 
@@ -51,8 +62,8 @@ impl Topic {
 }
 
 struct TgClient {
-    http_client: reqwest::Client,
-    request_url: String,
+    http_client:      reqwest::Client,
+    base_request_url: String,
 }
 
 impl TgClient {
@@ -63,14 +74,11 @@ impl TgClient {
             .build()
             .expect("Failed to build http client");
 
-        let request_url = format!(
-            "{}/bot{}/{}",
-            TELEGRAM_API_BASE_URL, secret, TELEGRAM_SEND_MESSAGE_METHOD
-        );
+        let base_request_url = format!("{}/bot{}", TELEGRAM_API_BASE_URL, secret);
 
         Self {
             http_client,
-            request_url,
+            base_request_url,
         }
     }
 
@@ -82,7 +90,10 @@ impl TgClient {
         text: &str,
     ) -> Result<reqwest::Response, reqwest::Error> {
         self.http_client
-            .post(&self.request_url)
+            .post(&format!(
+                "{}/{}",
+                self.base_request_url, TELEGRAM_SEND_MESSAGE_METHOD
+            ))
             .json(&SendMessagePayload::new(
                 recipient,
                 &format!(
@@ -96,7 +107,7 @@ impl TgClient {
             .await
     }
 
-    async fn notify_all(
+    async fn send_message_to_all(
         &self,
         recipients: &[String],
         topic: &str,
@@ -107,6 +118,61 @@ impl TgClient {
             recipients
                 .iter()
                 .map(|recipient| self.send_message(recipient, topic, sender, text))
+                .collect::<Vec<_>>(),
+        )
+        .await
+    }
+
+    async fn send_document(
+        &self,
+        recipient: &str,
+        topic: &str,
+        sender: &str,
+        message: &str,
+        filename: &str,
+        file_content: &[u8],
+    ) -> Result<reqwest::Response, reqwest::Error> {
+        let caption = format!(
+            "From: *{}@{}*\n\n{}",
+            *TgMarkdownString::new(sender),
+            topic,
+            message
+        );
+
+        let form = Form::new()
+            .text("chat_id", recipient.to_owned())
+            .text("caption", caption)
+            .text("parse_mode", TELEGRAM_MARKDOWN_V2_PARSE_MODE)
+            .part(
+                "document",
+                Part::bytes(file_content.to_owned()).file_name(filename.to_owned()),
+            );
+
+        self.http_client
+            .post(&format!(
+                "{}/{}",
+                self.base_request_url, TELEGRAM_SEND_DOCUMENT_METHOD
+            ))
+            .multipart(form)
+            .send()
+            .await
+    }
+
+    async fn send_document_to_all(
+        &self,
+        recipients: &[String],
+        topic: &str,
+        sender: &str,
+        message: &str,
+        filename: &str,
+        file_content: &[u8],
+    ) -> Vec<Result<reqwest::Response, reqwest::Error>> {
+        futures::future::join_all(
+            recipients
+                .iter()
+                .map(|recipient| {
+                    self.send_document(recipient, topic, sender, message, filename, file_content)
+                })
                 .collect::<Vec<_>>(),
         )
         .await
@@ -155,7 +221,7 @@ impl<'a> SendMessagePayload<'a> {
         Self {
             chat_id,
             text: text.to_owned(),
-            parse_mode: TELEGRAM_SEND_MESSAGE_PARSE_MODE,
+            parse_mode: TELEGRAM_MARKDOWN_V2_PARSE_MODE,
         }
     }
 }
@@ -176,12 +242,28 @@ async fn main() -> Result<(), std::io::Error> {
 
     env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
 
+    const MAIN_RESOURCE_PATH: &str = "/{topic_name}/{sender}";
+
     HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
             .app_data(topics_data.clone())
             .app_data(tg_data.clone())
-            .service(post_message)
+            .app_data(PayloadConfig::new(50 * 1000 * 1000))
+            .service(
+                web::resource(MAIN_RESOURCE_PATH)
+                    .guard(guard::fn_guard(|ctx| {
+                        ctx.header::<header::ContentType>()
+                            .map(|val| val.0.to_string().contains("multipart/form-data"))
+                            .unwrap_or(false)
+                    }))
+                    .route(web::post().to(post_message_with_document)),
+            )
+            .service(
+                web::resource(MAIN_RESOURCE_PATH)
+                    .guard(guard::Header("Content-Type", "text/plain"))
+                    .route(web::post().to(post_message)),
+            )
     })
     .workers(1)
     .bind(("0.0.0.0", config.port))?
@@ -190,17 +272,33 @@ async fn main() -> Result<(), std::io::Error> {
 }
 
 #[derive(Deserialize)]
-struct PostQuery {
+struct PostPathData {
     topic_name: String,
     sender:     String,
 }
 
-#[actix_web::post("/{topic_name}/{sender}")]
+fn extract_client_address(connection_info: ConnectionInfo) -> Result<IpAddr, HttpResponse> {
+    let client_address =
+        if let Some(ip_address_string) = connection_info.realip_remote_addr() {
+            match ip_address_string.parse() {
+                Ok(ip_address) => ip_address,
+                Err(_) =>
+                    return Err(HttpResponse::InternalServerError()
+                        .body("Cannot parse ip address from string")),
+            }
+        } else {
+            return Err(HttpResponse::InternalServerError()
+                .body("Cannot get ip address string from request"));
+        };
+
+    Ok(client_address)
+}
+
 async fn post_message(
     connection_info: ConnectionInfo,
     topics: web::Data<Arc<Topics>>,
     tg_client: web::Data<Arc<TgClient>>,
-    post_query: web::Path<PostQuery>,
+    post_query: web::Path<PostPathData>,
     message: String,
 ) -> impl Responder {
     let client_address: IpAddr =
@@ -219,11 +317,96 @@ async fn post_message(
     match topics.get(&post_query.topic_name) {
         Some(topic_info) if topic_info.is_allowed(client_address) => {
             let responses = tg_client
-                .notify_all(
+                .send_message_to_all(
                     &topic_info.recipients,
                     &post_query.topic_name,
                     &post_query.sender,
                     &message,
+                )
+                .await;
+
+            if responses.iter().all(|res| {
+                res.as_ref()
+                    .map_or_else(|_| false, |resp| resp.status() == StatusCode::OK)
+            }) {
+                HttpResponse::NoContent().finish()
+            } else {
+                HttpResponse::InternalServerError().body("bAdBaDnOtGoOd")
+            }
+        }
+        _ => HttpResponse::NotFound().body("No such topic"),
+    }
+}
+
+async fn post_message_with_document(
+    connection_info: ConnectionInfo,
+    topics: web::Data<Arc<Topics>>,
+    tg_client: web::Data<Arc<TgClient>>,
+    path_data: web::Path<PostPathData>,
+    mut multipart: actix_multipart::Multipart,
+) -> impl Responder {
+    let client_address = match extract_client_address(connection_info) {
+        Ok(client_address) => client_address,
+        Err(err_response) => return err_response,
+    };
+
+    let mut message = String::new();
+    let mut file_content = Vec::new();
+    let mut filename = String::new();
+
+    println!("here");
+    while let Some(item) = multipart.next().await {
+        let mut field = match item {
+            Ok(field) => field,
+            Err(err) => return HttpResponse::BadRequest().body(err.to_string()),
+        };
+
+        match field.name() {
+            "message" => {
+                let mut message_bytes_buffer: Vec<u8> = Vec::new();
+                while let Some(chunk) = field.next().await {
+                    let chunk_bytes = match chunk {
+                        Ok(chunk_bytes) => chunk_bytes,
+                        Err(err) => return HttpResponse::BadRequest().body(err.to_string()),
+                    };
+
+                    message_bytes_buffer.extend(chunk_bytes);
+                }
+
+                message = match String::from_utf8(message_bytes_buffer) {
+                    Ok(message) => message,
+                    Err(_) => return HttpResponse::BadRequest().body("Message is not valid UTF-8"),
+                }
+            }
+            "file" => {
+                filename = match field.content_disposition().get_filename() {
+                    Some(filename) => filename.to_owned(),
+                    None => return HttpResponse::BadRequest().body("Multipart filename missing"),
+                };
+                while let Some(chunk) = field.next().await {
+                    match chunk {
+                        Ok(chunk_bytes) => file_content.extend(chunk_bytes),
+                        Err(err) => return HttpResponse::BadRequest().body(err.to_string()),
+                    }
+                }
+            }
+            field_name =>
+                return HttpResponse::BadRequest()
+                    .body(format!("Unexpected mutlipart field \"{}\"", field_name)),
+        };
+    }
+
+
+    match topics.get(&path_data.topic_name) {
+        Some(topic_info) if topic_info.is_allowed(client_address) => {
+            let responses = tg_client
+                .send_document_to_all(
+                    &topic_info.recipients,
+                    &path_data.topic_name,
+                    &path_data.sender,
+                    &message,
+                    &filename,
+                    &file_content,
                 )
                 .await;
 
